740d496188e7105776a151c93a5307e61a004f09
Cody Touchet <cody.touchet@ibm.com>
2021-06-17 12:48:21 -0500

    dependencies updates

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const http = require('http');
 const debug = require('debug')('node-angular');
 const app = require('./backend/app');
+const express = require('express');
 
-const PORT = process.env.PORT || 3000;
+//Install express server
+const path = require('path');
 
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/mean-project'));
 
-app.listen(PORT, () => {
-	console.log("Listening on http://localhost:" + PORT);
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/mean-project/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
